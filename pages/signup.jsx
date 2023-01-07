b0a34ec7641e18bedcc5abe682a85166c21f9bfa
sanjay-29218 <sanjay29218@gmail.com>
2023-01-07 17:22:32 +0545

    minor login change

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -77,7 +77,7 @@ const Login = (props) => {
           <section className="text-gray-600 body-font flex ">
             <div className="container px-5   flex flex-wrap items-center justify-center ">
               <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col  w-full mt-10 md:mt-0 ">
-                <h2 className="text-gray-900 text-lg font-medium title-font mb-5">
+                <h2 className="text-gray-900 text-center md:text-[2rem] text-lg font-medium title-font mb-5">
                   Register
                 </h2>
                 <div className="relative mb-4">
